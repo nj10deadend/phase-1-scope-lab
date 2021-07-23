d6c228c2689bc5a4952bb177f3d50004956250e7
nj10deadend <nasjacks10@gmail.com>
2021-07-23 10:50:42 -0400

    commit tree cffd10dcaa5cf054433ba5c9f39d2ce457426793

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    let name = customerName.toUpperCase();
+    return name;
+} 
+function setBestCustomer(bC) {
+    var bC = 'not bob'
+    console.log(bc);
+    // var bestCustomer = 'not bob';
+    // return bestCustomer;
+}
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    // customerName = 'bob'.toUpperCase();
+    // return customerName; 
+
+    // let name = customerName.toUpperCase()
+    // console.log(name);
+    
+    // return name; 
+    // return customerName.toUpperCase();
